fa3ee68b91958565567a3c0969fbb45288834790
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-08-10 09:41:10 -0300

    ejercicio11

---
+++ ejercicio11.py
@@ -0,0 +1,16 @@
+from turtle import clear
+
+
+print ('-------------------------')
+print ('ejercicio 11: verificar si un alumno esta aprobado')
+print ('-------------------------')
+
+#entradas
+CAL=float(input('ingrese calificacion: '))
+
+#salida 
+print('\nSALIDA:')
+print("-------------------------")
+
+if CAL> 10:
+    print('aprobado')
