68fae03941891c20a65bc5ec33bbd6b4ae1be9ec
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-08-12 10:07:35 -0300

    corroborado

---
+++ ejercicio16.py
@@ -0,0 +1,30 @@
+from ast import If
+
+
+print("-------------------------------------------------------")
+print("ejercicio16 determinada la salida")
+print("-------------------------------------------------------")
+
+# entradas
+A = (input("ingrese A: "))
+B = (input("ingrese B: "))
+C = (input("ingrese C: "))
+
+# proceso
+print("-------------------------------------------------------")
+if A > B:
+    if A > C:
+        if B > C:
+            print(A, B, C)
+        else:
+            print(A, C, B)
+    else:
+        print(C, A, B)
+        else:
+        if B > C:
+            if A > C:
+                print(B, A, C)
+            else:
+                print(B, C, A)
+            else:
+            print(B, C, A)
